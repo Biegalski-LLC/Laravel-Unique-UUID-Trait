b0013a14f5cf635f6a63532e3f4e97da99886ab6
Michael Biegalski <michael@biegalski-llc.com>
2020-10-17 00:51:09 -0400

    first commit

---
+++ app/Traits/UuidTrait.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
+
+/**
+ * Trait UuidTrait
+ * @package App\Traits
+ */
+trait UuidTrait
+{
+    /**
+     * @return string
+     */
+    public function generateUuid(): string
+    {
+        return (string) Str::uuid();
+    }
+
+    /**
+     * @param string $model
+     * @param string $column
+     * @return string
+     * @throws \Exception
+     */
+    public function generateModelUuid(string $model, string $column = 'uuid'): string
+    {
+        do {
+            $uuid = $this->generateUuid();
+        } while (
+            DB::table($model)->where($column, '=', $uuid)->first() instanceof DB
+        );
+
+        return $uuid;
+    }
+}
